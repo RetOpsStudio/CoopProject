aa95296a6409ada97497c2321ac269f590a03086
Gar2d2 <pghost286@gmail.com>
2022-04-18 20:41:06 +0200

    *empty project commit(#master)

---
+++ Source/CoopProject/CoopProjectGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "CoopProjectGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ACoopProjectGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ACoopProjectGameMode();
+};
+
+
+
