a120bcb999278c9dd85a568c5fa1db4c117808ac
Gar2d2 <pghost286@gmail.com>
2022-08-16 19:38:10 +0200

    *prototype of grabber implemented, grabber can be adjusted in blueprints

--- Source/CoopProject/Private/GAS/GAUse.cpp
+++ Source/CoopProject/Private/GAS/GAUse.cpp
@@ -3,8 +3,10 @@
 
 #include "GAS/GAUse.h"
 #include "Kismet/KismetSystemLibrary.h"
+#include "Kismet/KismetMathLibrary.h"
 #include "GameFramework/Character.h"
 #include "Items/ItemBase.h"
+#include <algorithm>
 
 UGAUse::UGAUse() : Super()
 {
@@ -13,29 +15,60 @@ UGAUse::UGAUse() : Super()
 
 void UGAUse::ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData)
 {
-	//TODO refactor this, cause now its only prototype
-	auto location = ActorInfo->OwnerActor->GetActorLocation();
-	TArray<TEnumAsByte<EObjectTypeQuery> > ObjectTypes{ 
-		UEngineTypes::ConvertToObjectType(ECollisionChannel::ECC_WorldDynamic),
-	};
-	TArray< FHitResult > OutHits;
-	UKismetSystemLibrary::SphereTraceMultiForObjects(GetWorld(), location - FVector(0, 0, 40), location + FVector(0, 0, 40), 50, ObjectTypes, false, {}, EDrawDebugTrace::Persistent, OutHits, true);
-
-	for (const auto& hit : OutHits)
-	{
-		AItemBase* itemHitted = Cast<AItemBase>(hit.GetActor());
-		if (IsValid(itemHitted))
-		{
-			
-			itemHitted->UseItem(*ActorInfo);
-			break;
-		}
-		UE_LOG(LogTemp, Log, TEXT("hitted actor %s."), *hit.ToString());
-	}
+	GrabberPrototype_InFrontOfActor(ActorInfo);
 
 	EndAbility(Handle, ActorInfo, ActivationInfo, true, false);
 }
 
+void UGAUse::GrabberPrototype_InFrontOfActor(const FGameplayAbilityActorInfo* ActorInfo)
+{
+	//TODO refactor this, cause now its only prototype
+	auto actor = ActorInfo->OwnerActor;
+	auto location = actor->GetActorLocation();
+	FVector forwardVect = actor->GetActorForwardVector();
+	//add forward offset
+	location += forwardVect * m_forwardOffset;
+	FVector heightVector = FVector::UnitZ() -  UKismetMathLibrary::ProjectVectorOnToVector(FVector::UnitZ(), forwardVect);
+	//should be normalized anyway but...
+	heightVector.Normalize();
+
+	TArray<TEnumAsByte<EObjectTypeQuery> > ObjectTypes{
+		UEngineTypes::ConvertToObjectType(ECollisionChannel::ECC_WorldDynamic),
+	};
+
+	FVector extendHeight = heightVector * m_height;
+	FVector centerPoint = location + heightVector * m_centerHeight;
+
+	TArray< FHitResult > outHits;
+	UKismetSystemLibrary::SphereTraceMultiForObjects(GetWorld(), centerPoint - extendHeight/2, centerPoint - extendHeight/2, m_radius, ObjectTypes, false, {}, m_bDrawDebug ? EDrawDebugTrace::Persistent : EDrawDebugTrace::None, outHits, true);
+
+	if (!outHits.IsEmpty())
+	{
+		//TODO life quality - implement std::min_element in some library
+		//find closest Hit
+		AItemBase* closestItem = nullptr;
+
+		double minDist = DBL_MAX;
+		for (int i = 0; i < outHits.Num();i++)
+		{
+			auto& hit = outHits[i];
+
+			AItemBase* itemHitted = Cast<AItemBase>((hit).GetActor());
+			if (IsValid(itemHitted) && outHits[i].Distance < minDist)
+			{
+				closestItem = itemHitted;
+				minDist = hit.Distance;
+			}
+		}
+
+		if (IsValid(closestItem))
+		{
+			closestItem->UseItem(*ActorInfo);
+		}
+
+		UE_LOG(LogTemp, Log, TEXT("hitted actor %s."), *closestItem->GetName());
+	}
+}
 void UGAUse::EndAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, bool bReplicateEndAbility, bool bWasCancelled)
 {
 	Super::EndAbility(Handle, ActorInfo, ActivationInfo, bReplicateEndAbility, bWasCancelled);
--- Source/CoopProject/Public/GAS/GAUse.h
+++ Source/CoopProject/Public/GAS/GAUse.h
@@ -15,7 +15,27 @@ class COOPPROJECT_API UGAUse : public UPlayerGameplayAbilityBase
 	GENERATED_BODY()
 public:
 	UGAUse();
+
 protected:
+	virtual void GrabberPrototype_InFrontOfActor(const FGameplayAbilityActorInfo* ActorInfo);
 	virtual void ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData) override;
 	virtual void EndAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, bool bReplicateEndAbility, bool bWasCancelled) override;
+
+protected:
+	//TODO find sensible default values
+	//how much center will be rised comparing to actor location
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"), Category = "Properties")
+	double m_centerHeight = 0;
+	//how much center will be pushed frontward/backward of actor
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"), Category = "Properties")
+	double m_forwardOffset = 0;
+	//radius of sphere
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"), Category = "Properties")
+	double m_radius = 0;
+	//height of sphere
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"), Category = "Properties")
+	double m_height = 0;
+	//draw debug?
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"), Category = "Properties")
+	double m_bDrawDebug = true;
 };
